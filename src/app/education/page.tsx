69c0255f9fc88b6dcb05515b4def8b56927bceb9
Akash Misra <akashthem@gmail.com>
2024-02-20 13:46:44 -0500

    Added basic layout of education page and button for vocoder basics education

--- src/app/education/page.tsx
+++ src/app/education/page.tsx
@@ -31,16 +31,27 @@ export default function Home() {
         <main className="flex flex-col h-screen">
           <header className="flex justify-between p-4 bg-blue-300 drop-shadow">
             <div className="flex items-center">
-              <p className="mr-8 font-bold text-slate-800">Vocoder Education</p>
               {/* implement using NextUI Dropdown */}
               <Button className="bg-blue-700 text-slate-300">File</Button>
             </div>
+            <h1 className="mr-8 font-bold text-slate-800">Learn More About Vocoders!</h1>
             <div className="flex">
               <SignStat /> &nbsp;&nbsp;
             </div>
           </header>
           <div className="flex-grow p-4">
-
+            <div className="flex-col text-center">
+              <h2 className="mr-8 text-slate-800">Use the links below to navigate 
+                to helpful outside resources on vocoders and other components of music production
+              </h2>
+              &nbsp;
+              <div className="flex-row">
+                <Button className="bg-blue-700 text-slate-300">Vocoder Basics</Button>
+                &nbsp;
+                &nbsp;
+                <Button className="bg-blue-700 text-slate-300">Test</Button>
+              </div>
+            </div>
           </div>
           <footer className="p-4">
             <Button color="secondary" className="float-right">
